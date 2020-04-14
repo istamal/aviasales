85b9419ba910687ba01a9ff9cd8a34000942cedd
istamal <dagalaev@gmail.com>
2020-04-14 18:09:16 +0300

    Updates

--- precache-manifest.d0e0ead78532938fcac4bf10dd064aab.js
+++ precache-manifest.d0e0ead78532938fcac4bf10dd064aab.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c568e054529f655d1c29b7d59af7aa8f",
+    "revision": "7bb287d3b2c3e349ab7305e00338a036",
     "url": "/github.io/aviasales/index.html"
   },
   {
-    "revision": "8797173dcdf72a15cbee",
+    "revision": "eb3ec5cc440ced71503e",
     "url": "/github.io/aviasales/static/css/main.91fc976e.chunk.css"
   },
   {
-    "revision": "814b2065307798b3c364",
-    "url": "/github.io/aviasales/static/js/2.f64d6d5c.chunk.js"
+    "revision": "af79b21e338e916125fc",
+    "url": "/github.io/aviasales/static/js/2.e31fdc80.chunk.js"
   },
   {
     "revision": "64bea2e190f27355a76963b8cb94bb7b",
-    "url": "/github.io/aviasales/static/js/2.f64d6d5c.chunk.js.LICENSE.txt"
+    "url": "/github.io/aviasales/static/js/2.e31fdc80.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8797173dcdf72a15cbee",
-    "url": "/github.io/aviasales/static/js/main.62410ecf.chunk.js"
+    "revision": "eb3ec5cc440ced71503e",
+    "url": "/github.io/aviasales/static/js/main.06ed9c06.chunk.js"
   },
   {
     "revision": "b2d6ce9ef651d9cbcdf5",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/aviasales/precache-manifest.ee4ea4e3249e28e06c30e21ea11aba0a.js"
+  "/github.io/aviasales/precache-manifest.d0e0ead78532938fcac4bf10dd064aab.js"
 );
 
 self.addEventListener('message', (event) => {
