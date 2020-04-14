c5a08f3871f450035e0ba8fe337d90d114214d47
istamal <dagalaev@gmail.com>
2020-04-14 17:12:34 +0300

    Updates

---
+++ precache-manifest.ee4ea4e3249e28e06c30e21ea11aba0a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c568e054529f655d1c29b7d59af7aa8f",
+    "url": "/github.io/aviasales/index.html"
+  },
+  {
+    "revision": "8797173dcdf72a15cbee",
+    "url": "/github.io/aviasales/static/css/main.91fc976e.chunk.css"
+  },
+  {
+    "revision": "814b2065307798b3c364",
+    "url": "/github.io/aviasales/static/js/2.f64d6d5c.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/github.io/aviasales/static/js/2.f64d6d5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8797173dcdf72a15cbee",
+    "url": "/github.io/aviasales/static/js/main.62410ecf.chunk.js"
+  },
+  {
+    "revision": "b2d6ce9ef651d9cbcdf5",
+    "url": "/github.io/aviasales/static/js/runtime-main.faf366d6.js"
+  }
+]);
